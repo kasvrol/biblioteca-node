a27a94e4251d555778f6a7695651b9fab39202e5
kasvrol <dovalekarolina@gmail.com>
2022-07-12 21:21:10 -0300

    validando links

---
+++ cli.js
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+import pegaArquivo from "./index.js";
+import validaURLs from "./http.js";
+
+const caminho = process.argv;
+
+// função assincrona que deve esperar a função pegaArquivo ler o texto primeiro para trazer resultados
+async function processaTexto(caminhoDoArquivo) {
+    const resultado = await pegaArquivo(caminhoDoArquivo[2]);
+    if (caminho[3] === "validar") {
+        console.log(
+            chalk.yellow("links validados"),
+            await validaURLs(resultado)
+        );
+    } else {
+        console.log(chalk.yellow("lista de links"), resultado);
+    }
+}
+
+processaTexto(caminho);

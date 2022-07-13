e28fc398a8e0736166a69308098d4f6a80301355
kasvrol <dovalekarolina@gmail.com>
2022-07-12 22:03:04 -0300

    testando se a função pegaArquivo retorna corretamente

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,19 @@
 import pegaArquivo from "../index";
 
+const resultado = [
+    {
+        FileList: "https://developer.mozilla.org/pt-BR/docs/Web/API/FileList",
+    },
+];
+
 describe("pegaArquivo::", () => {
     it("deve ser uma função", () => {
         expect(typeof pegaArquivo).toBe("function");
+    });
+    it("deve retornar um array de resultados", async () => {
+        const theFunction = await pegaArquivo(
+            "../biblioteca-node/test/arquivos/com-link.md"
+        );
+        expect(theFunction).toEqual(resultado);
     });
 });
